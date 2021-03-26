d7fb4bad2b401d1c835227ee85cb176745324105
Trabelsi Mariem <61707024+mimi204@users.noreply.github.com>
2021-03-26 11:48:51 +0000

    secondecommit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ImageInScr from "./ImageInScr.png"
-
+import "./App.css"
 
 function App() {
   return (
@@ -8,17 +8,21 @@ function App() {
       
 <div>
   <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
-          <h1 className="title red">WELCOME</h1>
+          <h1 className="title red" class="rounded mx-auto d-block">Learn javascript with Codemycode</h1>
     <br />
-           <img width={200}  src={ImageInScr}   class="rounded float-start" alt ='imageInScr' />
+           <img width={200}  src={ImageInScr}   class="rounded mx-auto d-block"  alt ='imageInScr' />
           <br />
-          <img  width={200} src="ImageInPublic.png" class="rounded float-end" alt="myimageInPublic" />
-          
+          <br />
+          <img  width={200} src="ImageInPublic.png" class="rounded mx-auto d-block" alt="myimageInPublic" />
+      
+        </div>
+        
 
-  </div>
-  <video width={320} height={240} controls>
-    <source src="myVideo.mp4" type="video/mp4" />
-  </video>
+        <br />
+          <br />
+       <iframe width={560} height={315} class="rounded mx-auto d-block" src="https://www.youtube.com/embed/XewKPAClkSw" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+
+ 
 </div>
 
 
