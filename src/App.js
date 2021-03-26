18aa7c4a60072c32386058a003ea895175db55a6
Trabelsi Mariem <61707024+mimi204@users.noreply.github.com>
2021-03-26 11:19:33 +0000

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ImageInScr from "./ImageInScr.png"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+<div>
+  <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+          <h1 className="title red">WELCOME</h1>
+    <br />
+           <img width={200}  src={ImageInScr}   class="rounded float-start" alt ='imageInScr' />
+          <br />
+          <img  width={200} src="ImageInPublic.png" class="rounded float-end" alt="myimageInPublic" />
+          
+
+  </div>
+  <video width={320} height={240} controls>
+    <source src="myVideo.mp4" type="video/mp4" />
+  </video>
+</div>
+
+
+
     </div>
   );
 }
